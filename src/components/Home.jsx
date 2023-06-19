417046e83b0eeffc4d93497ce84f9d63af12a575
rodrigoc89 <rodrigojoaquinc89@gmail.com>
2023-06-19 11:29:28 -0300

    add title and icon index

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,6 +15,12 @@ const Home = () => {
           <a href="#footer" className="cta-btn">
             Contacto
           </a>
+          <a
+            href="https://www.canva.com/design/DAFgdcs6Ggk/88aqTqF8IOt7HzfZOZNZYQ/view?utm_content=DAFgdcs6Ggk&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelinkr"
+            target="_blank"
+            className="cta-btn">
+            CV
+          </a>
         </div>
       </section>
     </>
